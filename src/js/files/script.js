60df4a2e4a72a1c3223b43d059325ddd311d1144
tiwwwka <jolyfish23@gmail.com>
2023-05-31 11:04:55 +0200

    initial commit

---
+++ src/js/files/script.js
@@ -0,0 +1,18 @@
+// Підключення функціоналу "Чертоги Фрілансера"
+import { isMobile } from './functions.js';
+// Підключення списку активних модулів
+import { flsModules } from './modules.js';
+
+const checkSlider = document.querySelector('.advantages__slider ');
+const designerContent = document.querySelector('.advantages__item--designers');
+const influencersContent = document.querySelector('.advantages__item--influencers');
+checkSlider.addEventListener('click', () => {
+  checkSlider.classList.toggle('_active');
+  if (checkSlider.classList.contains('_active')) {
+    designerContent.classList.add('_hide');
+    influencersContent.classList.add('_active');
+  } else {
+    designerContent.classList.remove('_hide');
+    influencersContent.classList.remove('_active');
+  }
+});
